e971a2083281b8477734417d7168f95516e15bdd
Dick Lieber <dick@u505.com>
2023-01-10 15:49:10 -0600

    using recreated github

--- gh-cleanup-releases-tags.sh
+++ gh-cleanup-releases-tags.sh
@@ -3,9 +3,10 @@
 # jq from https://stedolan.github.io/jq/
 # gh from https://cli.github.com/manual/tagg
 for num in `gh release list 2>/dev/null | awk '{print $1}'`; do
+  echo "gh release delete $num -y"
   gh release delete $num -y
 done
-
+gh release list | sed 's/|/ /' | awk '{print $1, $8}' | while read -r line; do gh release delete -y "$line"; done
 git tag -d $(git tag -l)
 git fetch
 # Note: pushing once should be faster than multiple times
